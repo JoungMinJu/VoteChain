363ecf85c359925dcd6cf8cd4acfad197e69ac4d
JoungMinJu <alswn9938@gmail.com>
2026-02-02 17:19:53 +0900

    배포 스크립트 작성

---
+++ contracts/Deploy.s.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../contracts/Voting.sol";
+
+contract DeployVoting is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        Voting voting = new Voting();
+
+        console.log("Voting contract deployed to : ", address(voting));
+
+        vm.stopBroadcast();
+    }
+}
